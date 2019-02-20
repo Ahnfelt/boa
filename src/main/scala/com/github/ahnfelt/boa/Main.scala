054d76c1a322c174da0078ec7b63332c9aefc2ae
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2019-02-20 22:47:29 +0100

    Overload pre-filtering in the resolver based on number of parameters and rest parameters.

--- src/main/scala/com/github/ahnfelt/boa/Main.scala
+++ src/main/scala/com/github/ahnfelt/boa/Main.scala
@@ -1,7 +1,5 @@
 package com.github.ahnfelt.boa
 
-import scala.io.Source
-
 object Main {
     def main(arguments : Array[String]) : Unit = {
         val boaFile = new Importer().importAndProcess(arguments(0))
